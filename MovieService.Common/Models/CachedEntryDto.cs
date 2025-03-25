6a1132bf9e743b7fefef066e2313becadb1db7d4
Werner Cloete <wernerc101@gmail.com>
2025-03-25 18:46:43 +0200

    some additional changes and fixes

---
+++ MovieService.Common/Models/CachedEntryDto.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace MovieService.Common.Models
+{
+    public class CachedEntryDto
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Year { get; set; }
+        public string Genre { get; set; }
+        public string Director { get; set; }
+        public string Actors { get; set; }
+        public string Plot { get; set; }
+        public string Poster { get; set; }
+        public DateTime CachedAt { get; set; }
+    }
+}
